2183c7ef81322194a7567eb470f3ebe561b2e54d
Lionel Rowe <lionel.rowe@gmail.com>
2019-03-03 19:19:36 +0800

    security fix - remove xss vuln by properly sanitizing html output

--- client/app/helpers/formatMarkdown.js
+++ client/app/helpers/formatMarkdown.js
@@ -1,6 +1,7 @@
 import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/template';
 import marked from 'marked';
+import insane from 'insane';
 import * as hljs from 'highlightjs';
 
 import safeURI from './safeURI';
@@ -8,11 +9,60 @@ import safeURI from './safeURI';
 const liveRenderer = new marked.Renderer();
 const deadRenderer = new marked.Renderer();
 
-const checkbox = (checked) => {
-  // <label> self-closes before </li>
-  // TODO: enable (must be reliable against dupes)
-  return `<label><input type="checkbox" ${checked ? 'checked ' : ''} disabled> `;
+const insaneOptions = {
+  allowedAttributes: {
+    a: ['href', 'name', 'target', 'rel', 'title', 'tabindex'],
+    img: ['src', 'alt', 'title'],
+    input: ['type', 'checked', 'disabled'],
+    code: ['class'],
+    span: ['class'],
+    th: ['align'],
+    tr: ['align']
+  },
+  allowedClasses: {
+    ul: ['checklist'],
+    a: ['unstyled-link'],
+  },
+  allowedSchemes: ['http', 'https', 'mailto'],
+  allowedTags: [
+    'a', 'article', 'b', 'blockquote', 'br', 'caption', 'code', 'del', 'details', 'div', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'input', 'ins', 'kbd', 'label', 'li', 'main', 'ol', 'p', 'pre', 'section', 'span', 'strike', 'strong', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'th', 'thead', 'tr', 'u', 'ul'
+  ],
+  filter: node => {
+    [
+      [ 'code', 'language-' ],
+      [ 'span', 'hljs-' ]
+    ].forEach(([ tag, prefix ]) => {
+      if (
+        node.tag === tag
+        && typeof node.attrs.class === 'string'
+        && (/\s/.test(node.attrs.class) || !node.attrs.class.startsWith(prefix))
+      ) {
+        delete node.attrs.class;
+      }
+    });
+
+    return true;
+  },
+  transformText: null
 };
+
+for (let i = 1; i <= 6; i++) {
+  insaneOptions.allowedClasses[`h${i}`] = ['heading'];
+  insaneOptions.allowedAttributes[`h${i}`] = ['id'];
+}
+
+const sanitize = html => insane(html, insaneOptions);
+
+const checkbox = (checked) => {
+  // TODO: enable (must be reliable against dupes)
+  return `<label><input type="checkbox" ${checked ? 'checked ' : ''}disabled> `;
+};
+
+const listitem = text => {
+  const closer = text.trim().startsWith('<label>') ? '</label>' : '';
+
+  return `<li>${text}${closer}</li>\n`;
+}
 
 const list = (body, ordered, start) => {
   const type = ordered ? 'ol' : 'ul';
@@ -29,6 +79,7 @@ const list = (body, ordered, start) => {
 
 [liveRenderer, deadRenderer].forEach(renderer => {
   renderer.checkbox = checkbox;
+  renderer.listitem = listitem;
   renderer.list = list;
 });
 
@@ -79,19 +130,22 @@ marked.setOptions({
   gfm: true,
   tables: true,
   breaks: false,
-  sanitize: false, // covered by htmlSafe
+  sanitize: false, // covered by insane-powered `sanitize` function
   smartLists: true,
   smartypants: true,
   xhtml: false
 });
 
+
 const formatMarkdown = ([ markdown, isLive=true ]) => {
+
+  markdown = markdown.replace(/\[\[(.+?)\]\]/g, '<kbd>$1</kbd>');
 
   marked.setOptions({
     renderer: isLive ? liveRenderer : deadRenderer
   })
 
-  return htmlSafe(marked(/*parseChecklists(*/markdown/*)*/));
+  return htmlSafe(sanitize(marked(markdown)));
 };
 
 export default helper(formatMarkdown);
