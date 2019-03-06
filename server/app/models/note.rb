76aa044bff5fd49346d41fbefb3bf7d49ce7ad26
Lionel Rowe <lionel.rowe@gmail.com>
2019-03-06 17:19:56 +0800

    add max-length validation for Note model; allow empty strings but not null

--- server/app/models/note.rb
+++ server/app/models/note.rb
@@ -2,8 +2,8 @@ class Note < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
   validates :owner, presence: true
-  validates :title, presence: true # but can be empty string
-  validates :content, presence: true # but can be empty string
+  validates :title, length: { in: 0..255, allow_nil: false }
+  validates :content, length: { in: 0..65536, allow_nil: false }
 
   # scope :mine, -> { where(owner: current_user) }
 
